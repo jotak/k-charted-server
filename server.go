0be1c03b47158603a6771cd30ea04ba5ba46b803
Joel Takvorian <jtakvori@redhat.com>
2019-06-14 23:06:25 +0200

    Fetch items grouped by label

--- server.go
+++ server.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/kiali/k-charted/config"
 	khttp "github.com/kiali/k-charted/http"
 	"github.com/kiali/k-charted/model"
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	pmod "github.com/prometheus/common/model"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -32,6 +38,56 @@ var cfg = config.Config{
 		fmt.Printf(s+"\n", args...)
 	},
 	PodsLoader: podsLoader,
+}
+
+type grouped struct {
+	Namespace string   `json:"namespace"`
+	Items     []string `json:"items"`
+}
+
+func searchLabels(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	groupBy := pathParams["groupBy"]
+
+	p8s, err := api.NewClient(api.Config{Address: cfg.PrometheusURL})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	api := v1.NewAPI(p8s)
+	// Arbitrarily set time range. Meaning that discovery works with metrics produced within last hour
+	end := time.Now()
+	start := end.Add(-time.Hour)
+	results, err := api.Series(context.Background(), []string{"up"}, start, end)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	groups := []grouped{}
+	for _, labelSet := range results {
+		if namespace, ok := labelSet["namespace"]; ok {
+			if item, ok2 := labelSet[pmod.LabelName(groupBy)]; ok2 {
+				groups = addToGroup(groups, string(namespace), string(item))
+			}
+		}
+	}
+	respondWithJSON(w, http.StatusOK, groups)
+}
+
+func addToGroup(groups []grouped, namespace string, item string) []grouped {
+	for i, g := range groups {
+		if g.Namespace == namespace {
+			for _, other := range g.Items {
+				if other == item {
+					// Avoid duplicates
+					return groups
+				}
+			}
+			groups[i].Items = append(g.Items, item)
+			return groups
+		}
+	}
+	return append(groups, grouped{Namespace: namespace, Items: []string{item}})
 }
 
 func searchDashboards(w http.ResponseWriter, r *http.Request) {
@@ -65,8 +121,28 @@ func podsLoader(namespace, labels string) ([]model.Pod, error) {
 	return pods, nil
 }
 
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		response, _ = json.Marshal(map[string]string{"error": err.Error()})
+		code = http.StatusInternalServerError
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_, err = w.Write(response)
+	if err != nil {
+		cfg.Errorf("could not write response: %v", err)
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.HandleFunc("/groupBy/{groupBy}", searchLabels)
 	r.HandleFunc("/namespaces/{namespace}/dashboards", searchDashboards)
 	r.HandleFunc("/namespaces/{namespace}/dashboards/{dashboard}", getDashboard)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/react/build/")))
--- web/react-ts/src/components/Menu.tsx
+++ web/react-ts/src/components/Menu.tsx
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Nav, NavList, NavItem, PageSidebar } from '@patternfly/react-core';
+import { Nav, NavExpandable, NavList, NavItem, PageSidebar } from '@patternfly/react-core';
 
 type MenuState = {
-  activeItem?: string;
-  namespaces: string[];
+  activeItem?: string,
+  groupBy: string,
+  groups: Group[]
 };
 
-const defaultGroupBy = 'pod_name';
+type Group = {
+  namespace: string,
+  items: string[]
+};
 
 export class Menu extends React.Component<{}, MenuState> {
   static contextTypes = {
@@ -17,18 +21,19 @@ export class Menu extends React.Component<{}, MenuState> {
 
   constructor(props: {}) {
     super(props);
-    this.state = { namespaces: [] };
+    this.state = { groupBy: 'pod_name', groups: [] };
   }
 
   componentDidMount = () => {
-    this.fetchNamespaces();
+    this.fetch();
   }
 
-  fetchNamespaces = () => {
-    axios.get(`/namespaces`).then(rs => {
-      this.setState({ namespaces: rs.data });
+  fetch = () => {
+    axios.get(`/groupBy/${this.state.groupBy}`).then(rs => {
+      this.setState({ groups: rs.data });
     }).catch(error => {
-      this.setState({ namespaces: ['ns1', 'ns2', 'bookinfo-runtimes', 'istio-system'] });
+      // TODO: alert on page
+      console.log(error);
     });
   }
 
@@ -37,17 +42,22 @@ export class Menu extends React.Component<{}, MenuState> {
       <>
         <div style={{ fontStyle: 'italic', fontWeight: 'bold', paddingLeft: 10 }}>
           Group by&nbsp;
-          <input type="text" value={defaultGroupBy} onChange={evt => {}} style={{ width: 150 }}/>
+          <input type="text" value={this.state.groupBy} onChange={evt => this.setState({ groupBy: evt.target.value }, this.fetch)} style={{ width: 150 }}/>
         </div>
         <Nav onSelect={item => this.setState({ activeItem: String(item.itemId) })} onToggle={() => {}} aria-label="Nav">
           <NavList>
-            <NavItem>
-            </NavItem>
-            {this.state.namespaces.map(ns => {
+            {this.state.groups.map(g => {
               return (
-                <NavItem isActive={this.state.activeItem === ns} key={ns} itemId={ns}>
-                  <Link to={`/ns/${ns}`}>{ns}</Link>
-                </NavItem>
+                <NavExpandable title={g.namespace} groupId={g.namespace} isActive={this.state.activeItem !== undefined && this.state.activeItem.startsWith(g.namespace + '/')} isExpanded={false}>
+                  {g.items.map(item => {
+                    const id = `${g.namespace}/${item}`;
+                    return (
+                      <NavItem isActive={this.state.activeItem === id} key={id} itemId={id}>
+                        <Link to={`/ns/${g.namespace}/${this.state.groupBy}/${item}`}>{item}</Link>
+                      </NavItem>
+                    );
+                  })}
+                </NavExpandable>
               );
             })}
           </NavList>
