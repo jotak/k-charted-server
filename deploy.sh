f3dc6154974193e1284699c098ef212dce5f5b91
Lucas Ponce <ponce.ballesteros@gmail.com>
2019-07-08 12:58:49 +0200

    Minimal changes to make it work

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,15 @@ if [[ "$1" != "" ]]; then
   NAMESPACE="$1"
 fi
 
+echo "Install CRDs"
+oc apply -f ./crd.yaml -n $NAMESPACE
+
+echo "Install templates"
+for T in $(ls dashboards)
+do
+    oc apply -f $T -n $NAMESPACE
+done
+
 sed "s/VAR_NAMESPACE/$NAMESPACE/" Permissions.yml | oc apply -n $NAMESPACE -f -
 oc apply -f ./Deployment.yml -n $NAMESPACE
 oc apply -f ./Service.yml -n $NAMESPACE
